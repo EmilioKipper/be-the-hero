d52836d68c6dec11ae04bb5c2b95b27aeff92cbe
kipper-e <emilio.kipper@rede.ulbra.br>
2020-03-26 18:17:35 -0300

    Front-end and Back-end

---
+++ Backend/src/database/migrations/20200324220540_create_ongs.js
@@ -0,0 +1,15 @@
+// up = o que eu quero que seja feito
+exports.up = function (knex) {
+    return knex.schema.createTable('ongs', (table) => {
+        table.string('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+    })
+};
+// down = deu bosta, o q q é pra fazer?
+exports.down = function (knex) {
+    return knex.schema.dropTable('ongs')
+};
